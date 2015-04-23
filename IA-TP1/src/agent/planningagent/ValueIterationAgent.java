4f44b4fc4d211e4cbe3c31cfd4e6330633505a13
SebDuret <sebastienduret@msn.com>
2015-04-23 17:39:59 +0200

    updateV

--- IA-TP1/src/agent/planningagent/ValueIterationAgent.java
+++ IA-TP1/src/agent/planningagent/ValueIterationAgent.java
@@ -7,8 +7,13 @@ import java.util.Random;
 
 import environnement.Action;
 import environnement.Etat;
+import environnement.IllegalActionException;
 import environnement.MDP;
 import environnement.gridworld.ActionGridworld;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import util.HashMapUtil;
 
 
 /**
@@ -20,7 +25,8 @@ import environnement.gridworld.ActionGridworld;
 public class ValueIterationAgent extends PlanningValueAgent{
 	//*** VOTRE CODE
 	
-
+        HashMapUtil v;
+        Double gamma;
 	
 	/**
 	 * 
@@ -31,7 +37,9 @@ public class ValueIterationAgent extends PlanningValueAgent{
 		super(mdp);
 		
 		//*** VOTRE CODE
-	
+                this.gamma = gamma;
+                this.v = new HashMapUtil();
+                reset();
 	
 	}
 	
@@ -49,12 +57,49 @@ public class ValueIterationAgent extends PlanningValueAgent{
 	public void updateV(){
 		this.delta=0.0;
 		//*** VOTRE CODE
-		
-	
-		
+                
+                HashMapUtil v_clone = (HashMapUtil) this.v.clone();
+                // Pour chaque etats
+		for (Etat e : this.mdp.getEtatsAccessibles()){
+                    double max = 0;
+                    // Pour chaque action de cet etat
+                    for (Action a : this.mdp.getActionsPossibles(e)){
+                        Double sum = 0.0;
+                        try {
+                            Map<Etat, Double> etp_map = this.mdp.getEtatTransitionProba(e, a);
+                            // Realise la somme
+                            for (Etat ebis : etp_map.keySet()){
+                                sum += etp_map.get(ebis) * ( this.mdp.getRecompense(e, a, ebis) + this.gamma * v_clone.get(ebis) );
+                            }
+                        } catch (IllegalActionException ex) {
+                            Logger.getLogger(ValueIterationAgent.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (Exception ex) {
+                            Logger.getLogger(ValueIterationAgent.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        // Check la valeur max
+                        max = Math.max(max, sum);
+                    }
+                    // Mise a jour valeur
+                    this.v.put(e, max);
+                }
+                
+               // Mise a jour delta
+               Double diff_max = 0.0;
+               for (Etat e : this.v.keySet()){
+                   diff_max = Math.max(this.v.get(e), v_clone.get(e));
+               }
+               this.delta = diff_max;		
 		
 		// mise a jour vmax et vmin pour affichage
 		// ...
+               Double t_vmin = Double.MAX_VALUE;
+               Double t_vmax = Double.MIN_VALUE;
+               for (Double v_value : this.v.values()){
+                   t_vmin = Math.min(t_vmin, v_value);
+                   t_vmax = Math.max(t_vmax, v_value);
+               }
+               this.vmin = t_vmin;
+               this.vmax = t_vmax;
 		
 		//******************* a laisser a la fin de la methode
 		this.notifyObs();
@@ -74,8 +119,7 @@ public class ValueIterationAgent extends PlanningValueAgent{
 	@Override
 	public double getValeur(Etat _e) {
 		//*** VOTRE CODE
-		
-		return 0.0;
+		return this.v.get(_e);
 	}
 	/**
 	 * renvoi action(s) de plus forte(s) valeur(s) dans etat (plusieurs actions sont renvoyees si valeurs identiques, liste vide si aucune action n'est possible)
@@ -84,7 +128,14 @@ public class ValueIterationAgent extends PlanningValueAgent{
 	public List<Action> getPolitique(Etat _e) {
 		List<Action> l = new ArrayList<Action>();
 		//*** VOTRE CODE
-		
+                
+                // mes debuts mais c'est faux
+//		List<Action> t_l = this.mdp.getActionsPossibles(_e);
+//                Double max = 0.0;
+//                for (Action a : t_l){
+//                    max = Math.max(max, this.mdp.g)
+//                }
+//                
 		
 		return l;
 		
@@ -94,9 +145,13 @@ public class ValueIterationAgent extends PlanningValueAgent{
 	public void reset() {
 		super.reset();
 		//*** VOTRE CODE
+                // Initialise la map v
+		for (Etat e : this.mdp.getEtatsAccessibles()){
+                    this.v.put(e, 0.0);
+                }
 		
-		
-		
+		this.vmin = Double.MAX_VALUE;
+                this.vmax = Double.MAX_VALUE;
 		
 		
 		/*-----------------*/
@@ -108,7 +163,7 @@ public class ValueIterationAgent extends PlanningValueAgent{
 	@Override
 	public void setGamma(double arg0) {
 		//*** VOTRE CODE
-		
+		this.gamma = arg0;
 	}
 
 	
